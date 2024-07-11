e92de3848c4bd393a12dde500f80fce92749435d
default <mstrenk12@gmail.com>
2024-07-11 21:00:41 +0000

    Remove throw event and cleaned up strings

--- src/main/java/com/flyerzrule/enderpearl_logger/EnderPearlListener.java
+++ src/main/java/com/flyerzrule/enderpearl_logger/EnderPearlListener.java
@@ -1,13 +1,9 @@
 package com.flyerzrule.enderpearl_logger;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 
 public class EnderPearlListener implements Listener {
 
@@ -18,34 +14,21 @@ public class EnderPearlListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerUseEnderPearl(PlayerInteractEvent event) {
-        // Make sure the pearl is in the main hand and a right click
-        if (event.getHand() == EquipmentSlot.HAND &&
-            event.getAction().name() == "RIGHT_CLICK_AIR"
-        ) {
-            ItemStack item = event.getItem();
-            if (item != null && item.getType() == Material.ENDER_PEARL) {
-                plugin.getLogger().info(event.getPlayer().getName() + " used an Enderpearl at " + formatLocation(event.getPlayer().getLocation()));
-            }
-        }
-    }
-
-    @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (event.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             Location from = event.getFrom();
             Location to = event.getTo();
             double distance = from.distance(to);
 
-            plugin.getLogger().info(event.getPlayer().getName() + " traveled " + distance + " blocks with an Enderpearl. " +
-                    "Start: " + formatLocation(from) + ", End: " + formatLocation(to));
+            String logMessage = String.format("%s(%s) traveled %.2f blocks with an enderpearl. Start: %s, End: %s",
+                    event.getPlayer().getName(), event.getPlayer().getUniqueId().toString(), distance,
+                    formatLocation(from), formatLocation(to));
+            plugin.getLogger().info(logMessage);
         }
     }
 
     private String formatLocation(Location location) {
-        return "World: " + location.getWorld().getName() +
-               ", X: " + location.getX() +
-               ", Y: " + location.getY() +
-               ", Z: " + location.getZ();
+        return String.format("World: %s, X: %.2f, Y: %.2f, Z: %.2f", location.getWorld().getName(), location.getX(),
+                location.getY(), location.getZ());
     }
 }
